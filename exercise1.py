547d0bba93e0a2dffb789ab165912d1e25e8c172
zohaibsheikh007 <107213943+zohaibsheikh007@users.noreply.github.com>
2022-06-10 23:14:33 +0530

    3

---
+++ exercise1.py
@@ -0,0 +1,7 @@
+#create a dictionary and take input from the user and return the meaning of the word from the dictionary
+
+dict1 = {"mutable" : "changable" , "immutable" : "unchangable" , "biscuit" : "a crisp cake"}
+print(dict1)
+print(dict1.get("mutable"))
+
+
