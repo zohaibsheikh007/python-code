bc60dda262f253c0e744f417526fbdc0e6dcaf9d
zohaibsheikh007 <zohaibsheikh8080@gmail.com>
2022-06-16 19:02:32 +0530

    skip

---
+++ 02_.py
@@ -0,0 +1,22 @@
+'''dont 
+ remove 
+ this 
+line'''
+print("hello", "i am zohaib")
+# to escape sequence character use \\ while printing a path
+print("C:\"zohaib")
+print("zohaib is a \n good boy \t 1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
