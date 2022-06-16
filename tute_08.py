98b964010158209adbbc9aef453ff8af2a83bb49
zohaibsheikh007 <zohaibsheikh8080@gmail.com>
2022-06-16 18:43:44 +0530

    inbuilt methods

---
+++ tute_08.py
@@ -0,0 +1,17 @@
+mystr = "zohaibisagood2boy"
+print(len(mystr))
+print(mystr[::-1])
+
+print(mystr.isalnum())
+print(mystr.isalpha())
+print(mystr.endswith("boy"))
+print(mystr.count("b"))
+print(mystr.capitalize())
+print(mystr.find("is"))
+print(mystr.upper())
+print(mystr.replace("is","are"))
+
+
+
+
+
