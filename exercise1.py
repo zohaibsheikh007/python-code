91bdffdb0c48a0d05fdf3392d3122e11f586441e
zohaibsheikh007 <zohaibsheikh8080@gmail.com>
2022-06-17 15:07:28 +0530

    word meaning

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+#create a dictionary and take input from the user and return the meaning of the word from the dictionary
+
+dict1 = {"mutable" : "changable" , "immutable" : "unchangable" , "biscuit" : "a crisp cake"}
+a = input(" the meaning of ")
+
+print(dict1.get(a))
+
+
